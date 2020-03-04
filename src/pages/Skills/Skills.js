ef6b51d961bf02ff1d772801f7d2c9be2a5a7fe7
seanmcquaid <sean.mcquaid2@verizon.net>
2020-03-03 22:57:02 -0500

    set up skills section, just need to finish typing out the objects tommorrow and style the actual container

--- src/pages/Skills/Skills.js
+++ src/pages/Skills/Skills.js
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from "react";
 import { HeaderText, PageContainer, LeftContainer, RightContainer, ParagraphText, LoadingSpinner } from "components/components";
 import Aux from "hoc/Aux/Aux";
+import styled from "styled-components";
+import skillsList from "./skillsList";
 
 const Skills = () => {
     const [isLoading, setIsLoading] = useState(true);
@@ -16,15 +18,42 @@ const Skills = () => {
                 <Aux>
                     <LeftContainer>
                         <HeaderText>Skills</HeaderText>
-                        <ParagraphText>Text about Skills here</ParagraphText>
+                        <ParagraphText>I currently utilize in Java/Springboot at work, however, my speciality is in full-stack JavaScript development. I specifically have a passion for creating visually appealing, responsive applications. I currently spend most of my time outside of work learning new concepts and creating creative solutions in React and React Native.</ParagraphText>
                     </LeftContainer>
                     <RightContainer>
-                        Skills here
+                        <IconsContainer>
+                            {skillsList.map(skill => {
+                                const {name, Component} = skill;
+                                return (
+                                    <IconContainer>
+                                        <Component/>
+                                        <ParagraphText>{name}</ParagraphText>
+                                    </IconContainer>
+                                )
+                            })}
+                        </IconsContainer>
                     </RightContainer>
                 </Aux>
             }
         </PageContainer>
     )
 };
+
+const IconsContainer = styled.div`
+    display : flex;
+    flex-direction : flex-wrap;
+    justify-content : center;
+    align-items : center;
+    width : 100%;
+    padding : 1rem;
+`;
+
+const IconContainer = styled.div`
+    display : flex;
+    flex-direction : column;
+    justify-content : center;
+    align-items : center;
+    font-size : 1rem;
+`;
 
 export default Skills;
---
+++ src/pages/Skills/skillsList.js
@@ -0,0 +1,31 @@
+import {
+    DiJavascript, 
+    DiReact, 
+    DiNodejs, 
+    DiHtml5, 
+    DiCss3, 
+    DiMysql, 
+    DiPostgresql, 
+    DiMongodb, 
+    DiGit,
+    DiPython,
+    DiAws,
+    DiJqueryLogo
+} from "react-icons/di";
+
+const skillsList = [
+    {
+        name : "JavaScript",
+        Component : DiJavascript
+    },
+    {
+        name : "React",
+        Component : DiReact
+    },
+    {
+        name : "Node.js",
+        Component : DiNodejs
+    },
+]
+
+export default skillsList;
