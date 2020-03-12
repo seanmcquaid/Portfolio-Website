4e4377ff52b1563b081061828a699546fd5c2e82
seanmcquaid <sean.mcquaid2@verizon.net>
2020-03-12 19:30:32 -0400

    Updates

---
+++ precache-manifest.dd68293752b80d882a0fecbb825c4267.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3034dfbb5fdac55c8c54e55b8759b7fb",
+    "url": "/portfolio-website/index.html"
+  },
+  {
+    "revision": "03fa3c2eb4d833876b0b",
+    "url": "/portfolio-website/static/css/main.178ccadc.chunk.css"
+  },
+  {
+    "revision": "90e40ae876ee75039fe4",
+    "url": "/portfolio-website/static/js/2.22f2093b.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/portfolio-website/static/js/2.22f2093b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03fa3c2eb4d833876b0b",
+    "url": "/portfolio-website/static/js/main.e9f2210f.chunk.js"
+  },
+  {
+    "revision": "9916e352a78640b8162d",
+    "url": "/portfolio-website/static/js/runtime-main.ab58094c.js"
+  },
+  {
+    "revision": "332609d0080ccc926752e24780cec804",
+    "url": "/portfolio-website/static/media/Sean McQuaid - Resume.332609d0.pdf"
+  },
+  {
+    "revision": "83594d4104c4f7e8b3e234f19a894c0b",
+    "url": "/portfolio-website/static/media/dekTech.83594d41.jpg"
+  },
+  {
+    "revision": "0d3f9a3e9b1d1f319131f6a28c166889",
+    "url": "/portfolio-website/static/media/headshot.0d3f9a3e.jpg"
+  },
+  {
+    "revision": "1d04a8b84ea53edffb9e26ea4e441295",
+    "url": "/portfolio-website/static/media/trumpetHeadshot.1d04a8b8.jpg"
+  }
+]);
--- src/components/SubHeaderText/SubHeaderText.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export default styled.h2`
-    margin : 1rem;
-    padding : 0;
-    font-family: Rubik;
-    font-size : 1rem;
-`;
