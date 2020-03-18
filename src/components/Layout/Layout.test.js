c6cde405ba28d725af465854b9b71236f03f2af6
seanmcquaid <sean.mcquaid2@verizon.net>
2020-03-17 23:06:01 -0400

    wrote a couple simple unit tests but I realize now that react testing library is VERY different form enzyme. I will spend tomorrow digging into this library a bit more

--- src/components/Layout/Layout.test.js
+++ src/components/Layout/Layout.test.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {render} from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 import Layout from "./Layout";
 import {HashRouter as Router} from "react-router-dom";
 
@@ -7,5 +8,11 @@ describe("<Layout/>", () => {
     it("Snapshot matches", () => {
         const layout = render(<Router><Layout/></Router>);
         expect(layout).toMatchSnapshot();
+    });
+    
+    it("Children Display Properly", () => {
+        const children = "Children are here!";
+        const {queryByText} = render(<Router><Layout children={children}/></Router>);
+        expect(queryByText(children)).toHaveTextContent(children);
     });
 });
--- src/components/LinkButton/LinkButton.test.js
+++ src/components/LinkButton/LinkButton.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import LinkButton from "./LinkButton";
 import {HashRouter as Router} from "react-router-dom";
 import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 
 describe("<LinkButton/>", () => {
     it("Matches snapshot", () => {
@@ -11,5 +12,17 @@ describe("<LinkButton/>", () => {
         };
         const linkButton = render(<Router><LinkButton {...props}/></Router>);
         expect(linkButton).toMatchSnapshot();
+    });
+    it("Title displays properly", () => {
+        const props = {
+            title : "Test Link Button",
+            route : "/",
+        };
+        const {title} = props;
+        const {queryByText} = render(<Router><LinkButton {...props}/></Router>);
+        expect(queryByText(title)).toHaveTextContent(title);
+    });
+    it("Passed route is correct", () => {
+        // look up how to test routes passed to link components
     });
 });
