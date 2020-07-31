9198853333ef94aaec325e617f635695d9f063de
seanmcquaid <sean.mcquaid2@verizon.net>
2020-07-30 23:27:21 -0400

    removed suspense

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import React, {lazy, Suspense} from "react";
+import React, {lazy} from "react";
 import {HashRouter as Router, Switch, Route} from "react-router-dom";
-import {Layout, LoadingSpinner} from "components/components";
+import {Layout} from "components/components";
 
 const App = () => (
   <Router basename="/">
       <Layout>
-        <Suspense fallback={<LoadingSpinner isLoading={true}/>}>
-          <Switch>
-              <Route exact path="/" component={lazy(() => import("pages/Home/Home"))}/>
-              <Route exact path="/about" component={lazy(() => import("pages/About/About"))}/>
-              <Route exact path="/contact" component={lazy(() => import("pages/Contact/Contact"))}/>
-              <Route exact path="/skills" component={lazy(() => import("pages/Skills/Skills"))}/>
-              <Route exact path="/projects" component={lazy(() => import("pages/Projects/Projects"))}/>
-              <Route component={lazy(() => import("pages/Error/Error"))}/>
-          </Switch>
-        </Suspense>
+        <Switch>
+            <Route exact path="/" component={lazy(() => import("pages/Home/Home"))}/>
+            <Route exact path="/about" component={lazy(() => import("pages/About/About"))}/>
+            <Route exact path="/contact" component={lazy(() => import("pages/Contact/Contact"))}/>
+            <Route exact path="/skills" component={lazy(() => import("pages/Skills/Skills"))}/>
+            <Route exact path="/projects" component={lazy(() => import("pages/Projects/Projects"))}/>
+            <Route component={lazy(() => import("pages/Error/Error"))}/>
+        </Switch>
       </Layout>
   </Router>
 );
