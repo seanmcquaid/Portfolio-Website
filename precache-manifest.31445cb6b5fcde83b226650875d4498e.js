473b0294053ea22319e5c1211b6c3126d0967980
seanmcquaid <sean.m.mcquaid@gmail.com>
2021-01-11 21:29:47 +0000

    Updates

---
+++ precache-manifest.31445cb6b5fcde83b226650875d4498e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "999271d5ae382fa166d106d6b390d0d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73547e3168cee08b8bb6",
+    "url": "/static/css/main.fec187b6.chunk.css"
+  },
+  {
+    "revision": "6eaa453264dde2bf328b",
+    "url": "/static/js/2.3aa9f05c.chunk.js"
+  },
+  {
+    "revision": "eb56b19eba63a53416f4fd778023529f",
+    "url": "/static/js/2.3aa9f05c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73547e3168cee08b8bb6",
+    "url": "/static/js/main.4c1c7f24.chunk.js"
+  },
+  {
+    "revision": "35ec47dfee4c65c275be",
+    "url": "/static/js/runtime-main.c6e37e88.js"
+  },
+  {
+    "revision": "aecbbeed83c999fec4d86e09424ebf62",
+    "url": "/static/media/Sean McQuaid - Resume.aecbbeed.pdf"
+  },
+  {
+    "revision": "ea3a88878cb39941ab1807561693f9a0",
+    "url": "/static/media/headshot.ea3a8887.jpg"
+  },
+  {
+    "revision": "dae8dcce064b4f5e89ddd767c113328a",
+    "url": "/static/media/tableManagementSystem.dae8dcce.jpg"
+  },
+  {
+    "revision": "2ff60e43e564c7df70645aa670d3c9a4",
+    "url": "/static/media/trumpetHeadshot.2ff60e43.jpg"
+  },
+  {
+    "revision": "e3f46ed5ad35ac9e73a73865c0614aa5",
+    "url": "/static/media/webChat.e3f46ed5.jpg"
+  },
+  {
+    "revision": "6e03f79b57d7d4727c7b73c03f47bd30",
+    "url": "/static/media/whacAMole.6e03f79b.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ffb4381f099d40e7877b686293da3191.js"
+  "/precache-manifest.31445cb6b5fcde83b226650875d4498e.js"
 );
 
 self.addEventListener('message', (event) => {
