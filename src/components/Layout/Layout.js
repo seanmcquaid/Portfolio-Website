deebf7ae13b925f6b9f0d1b57743ff2558e43b01
seanmcquaid <sean.mcquaid2@verizon.net>
2021-01-21 16:44:27 -0500

    removed some unneeded CSS

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -10,9 +10,6 @@ const Layout = ({ children }) => (
 );
 
 const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
   height: 100%;
   width: 100%;
   @media (max-width: 975px) {
