a50eed48d917afd1e6db256798f4fb454ff64828
seanmcquaid <sean.mcquaid2@verizon.net>
2021-01-21 19:09:53 -0500

    removed normalize as it was useless

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 import reactAxe from '@axe-core/react';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Normalize } from 'styled-normalize';
 import App from './App';
 import './index.css';
 
 reactAxe(React, ReactDOM, 1000, {});
 
-const Root = () => (
-  <React.Fragment>
-    <Normalize />
-    <App />
-  </React.Fragment>
-);
-
-ReactDOM.render(<Root />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
