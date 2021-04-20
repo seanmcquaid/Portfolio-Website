52858d3784653e4ccada1aef2e0cb693d73e1b4c
seanmcquaid <sean.mcquaid2@verizon.net>
2021-04-19 21:45:52 -0400

    added new project

--- src/pages/Projects/projectsList.js
+++ src/pages/Projects/projectsList.js
@@ -1,6 +1,6 @@
 import tableManagementSystem from 'images/tableManagementSystem.jp2';
 import webFakeBook from 'images/webFakeBook.jp2';
-import whacAMole from 'images/whacAMole.jp2';
+import edhPodGenerator from 'images/edhPodGenerator.jp2';
 import webChat from 'images/webChat.jp2';
 
 export default [
@@ -11,6 +11,13 @@ export default [
       'Built a Web solution to a problem for college music students, having free access to chord changes for jazz standards with the MERN stack and TypeScript.',
     repoLink: 'https://github.com/seanmcquaid/Web-Fake-Book-Client',
     demoLink: 'https://web-fake-book-client.herokuapp.com/',
+  },
+  {
+    title: 'EDH Pod Generator',
+    image: edhPodGenerator,
+    description:
+      'Built with Vue.js on the Front End and Java/Springboot with PostGreSQL on the Back End, this is a tool that is meant to easily create play groups for your MTG nights!',
+    repoLink: 'https://github.com/seanmcquaid/EDH-Pod-Generator-Client',
   },
   {
     title: 'Table Management System',
@@ -25,12 +32,5 @@ export default [
     description:
       'Built a Web Chat app for users to communicate with people on their friends list in real time with the MERN stack and Socket.io',
     repoLink: 'https://github.com/seanmcquaid/Web-Chat',
-  },
-  {
-    title: 'Whac-A-Mole',
-    image: whacAMole,
-    description:
-      'Built with React/TypeScript on the Front End and Java/Springboot on the Back End, this is a recreation of the classic arcade game, Whac-A-Mole.',
-    repoLink: 'https://github.com/seanmcquaid/Whac-A-Mole',
   },
 ];
